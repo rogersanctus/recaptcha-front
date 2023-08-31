41f75cd5df921f9b13fe2f74c5ad04ee65b44bd4
rogersanctus <rogersanctus@gmail.com>
2023-08-31 02:23:40 -0300

    Improve cloudflare turnstile typing

--- @types/cloudflare-turnstile.d.ts
+++ @types/cloudflare-turnstile.d.ts
@@ -1,12 +1,17 @@
 declare global {
   interface RenderOptions {
     sitekey: string
-    callback: (token: string) => void
+    action?: string
+    retry?: 'never' | 'auto'
+    'retry-interval'?: number
+    callback?: (token: string) => void
+    'error-callback'?: (errorCode: string) => boolean
   }
 
   const turnstile: {
     ready: (onready: () => void) => void
-    render: (targetElement: string, options: RenderOptions) => void
+    render: (targetElement: string, options: RenderOptions) => string
+    remove: (widgetId: string) => void
   }
 }
 
