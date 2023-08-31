5a9a023d74b5133b0f98767588a4f2bc222584d0
rogersanctus <rogersanctus@gmail.com>
2023-08-31 02:22:48 -0300

    Adds typing for env variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  /** The site key for Cloudflare Turnstile captcha */
+  readonly VITE_TURNSTILE_SITEKEY: string
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
