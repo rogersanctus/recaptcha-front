40cb732c764b56672d10118398783fa9f83b7777
rogersanctus <rogersanctus@gmail.com>
2023-09-08 22:05:11 -0300

    Fix linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,13 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
@@ -17,4 +17,3 @@ module.exports = {
     ]
   }
 }
-
